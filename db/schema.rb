28cc9c07ddb90cd63482c3228a9019c8e289edbb
bohdanvakh <bohdanvakh@gmail.com>
2025-01-09 10:18:40 +0200

    Add attributes to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_05_152625) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_09_081658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_05_152625) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "phone_number"
   end
 
   add_foreign_key "components", "folders"

95e2b9975f2d2c182b89d51633a6f0cc10b836f4
bohdanvakh <bohdanvakh@gmail.com>
2025-01-12 15:51:35 +0200

    Add authorized! and current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  def authenticate!
+    token = request.headers['Authorization']&.split(' ')&.last
+
+    if token
+      begin
+        decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+        @current_user = User.find(decoded_token['user_id'])
+      rescue JWT::DecodeError
+        render json: { error: "Invalid or expired token" }, status: :unauthorized
+      end
+    else
+      render json: { error: "Token missing" }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
+  end
 end

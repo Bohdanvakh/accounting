84ddca05c427249d02eed932be7e7cd4e21b4ace
bohdanvakh <bohdanvakh@gmail.com>
2025-01-21 12:43:13 +0200

    Add destroy method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class SessionsController < ApplicationController
       before_action :find_user, only: [:authenticate_phone_number, :create]
+      before_action :authenticate!, only: [:destroy]
 
       def authenticate_phone_number
         if @user
@@ -16,6 +17,7 @@ module Api
         if @user && @user.phone_confirmation_token && @user.confirmed
           result = @user.confirm_phone(params[:confirmation_code])
           if result[:success]
+            current_user.update!(active: true)
             render json: { token: @user.generate_jwt_token,  message: 'Logged in successfully.' }, status: :ok
           else
             render json: { message: result[:message] }, status: :ok
@@ -23,6 +25,11 @@ module Api
         else
           render json: { error: 'Invalid credentials or unverified phone number.' }, status: :unauthorized
         end
+      end
+
+      def destroy
+        # in the frontend part of sign out logic we remove jwt token from local storage.
+        current_user.update!(active: false)
       end
 
       private
@@ -33,4 +40,3 @@ module Api
     end
   end
 end
-

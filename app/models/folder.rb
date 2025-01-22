2b6b5e5d9474d2943944db628ffa76bcc6f2dc02
bohdanvakh <bohdanvakh@gmail.com>
2025-01-22 16:18:12 +0200

    Add validations for folder name

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,5 +1,6 @@
 class Folder < ApplicationRecord
   belongs_to :user
   has_many :components, dependent: :destroy
-end
 
+  validates :name, presence: true, length: {minimum:6, maximum:120}, format: {with: /[a-zA-Z]/}
+end

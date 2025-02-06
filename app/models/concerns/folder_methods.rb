77f9ed830afa4e47ccdc8c2cae4cc2a5ebac8831
bohdanvakh <bohdanvakh@gmail.com>
2025-02-06 02:02:46 +0200

    Refactor methods

--- app/models/concerns/folder_methods.rb
+++ app/models/concerns/folder_methods.rb
@@ -2,15 +2,7 @@ module FolderMethods
   def components_with_dimension
     components.includes(:dimension).map do |component|
       {
-        id: component.id,
-        name: component.name,
-        folder: component.folder_id,
-        price: component.price,
-        code: component.code,
-        name: component.name,
-        wieght: component.wieght,
-        measurement: component.measurement,
-        currency: component.currency,
+        data: component,
         dimension: component.dimension
       }
     end

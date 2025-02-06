03b065803302e44bf4f544b45138b4dac877d67f
bohdanvakh <bohdanvakh@gmail.com>
2025-02-06 02:40:37 +0200

    Fix column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_02_150423) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_06_003313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_02_150423) do
     t.float "price", null: false
     t.integer "code"
     t.string "name", limit: 120, null: false
-    t.decimal "wieght", precision: 10, scale: 2, null: false
+    t.decimal "weight", precision: 10, scale: 2, null: false
     t.string "measurement", null: false
     t.string "currency", null: false
     t.index ["folder_id"], name: "index_components_on_folder_id"

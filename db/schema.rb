d9089ce7c53797f439c4ed7dc3140e89ed1783a0
bohdanvakh <bohdanvakh@gmail.com>
2025-02-15 19:53:13 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_06_120540) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_14_182522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,12 +28,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_06_120540) do
   end
 
   create_table "dimensions", force: :cascade do |t|
-    t.float "length", null: false
-    t.float "width", null: false
-    t.float "height", null: false
+    t.integer "length", null: false
+    t.integer "width", null: false
+    t.integer "height", null: false
     t.bigint "component_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "measurement"
     t.index ["component_id"], name: "index_dimensions_on_component_id", unique: true
   end
 

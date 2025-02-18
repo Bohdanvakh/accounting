748a80a6e17083f5866f0e93c641e2c16bc928bd
bohdanvakh <bohdanvakh@gmail.com>
2025-02-19 00:37:22 +0200

    Add scopes for name and code

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,10 +1,17 @@
 class Folder < ApplicationRecord
+  # Callbacks
+  before_save :set_code
+
   # Include concerns
   include FolderMethods
 
   # Associations
   belongs_to :user
   has_many :components, dependent: :destroy
+
+  # Scopes
+  scope :by_name, ->(name) { where("name = ?", name) }
+  scope :by_code, ->(code) { where("code = ?", code) }
 
   # Validations
   validates :name, presence: true, length: {minimum:6, maximum:120}, format: {with: /[a-zA-Z]/}
@@ -16,5 +23,9 @@ class Folder < ApplicationRecord
 
   def weight
     FolderWeightCalculator.new(self).calculate
+  end
+
+  def set_code
+    self.code = "F_#{DateTime.now.to_i}_U#{user_id}"
   end
 end

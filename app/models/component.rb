2f34715ac3c1011b0779682723302ea0e6581b76
bohdanvakh <bohdanvakh@gmail.com>
2025-02-26 19:04:50 +0200

    Add set_code to components

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,10 +1,13 @@
 class Component < ApplicationRecord
+  # Callbacks
+  before_save :set_code
+
   # Include concerns
   include ComponentMethods
   include Searchable
 
   # Scopes
-  include_searchable_scopes :by_name # includes scopes we want to use
+  include_searchable_scopes :by_name, :by_code # includes scopes we want to use
 
   # Associations
   belongs_to :folder
@@ -17,4 +20,11 @@ class Component < ApplicationRecord
   validates :name, presence: true, length: { maximum: 120 }
   validates :weight, presence: true
   validates :measurement, presence: true, inclusion: { in: %w(kg lb), message: "%{value} is not a valid measurement" }
+
+  # Methods
+  private
+
+  def set_code
+    self.code = "C_#{DateTime.now.to_i}_F#{folder_id}"
+  end
 end
